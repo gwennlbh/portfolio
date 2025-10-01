9fca28663b8818211174f7d3ea1871dda3d8a997
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-10-02 00:14:41 +0200

    🔧 Setup sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import math from "remark-math";
-import katex from "rehype-katex";
-
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import katex from "rehype-katex";
+import math from "remark-math";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    sitemap({
+      changefreq: "weekly",
+      // TODO wait until URLPattern lands in bun (and Node)
+      // filter: (url) => !new URLPattern("/works/:work", url).test(url)
+      filter: (url) => !new URL(url).pathname.startsWith("/works/"),
+    }),
+  ],
   site: `https://${process.env.LANG}.gwen.works`,
   markdown: {
     remarkPlugins: [math],
