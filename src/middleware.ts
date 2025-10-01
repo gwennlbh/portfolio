b9122f07a871f79697b70a008ae73796fa352afb
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-10-02 00:23:59 +0200

    🔇 Remove useless logs in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,83 +5,61 @@ import { collections } from "./content.config.ts";
 
 let loggedCollections = false;
 
-export const onRequest = defineMiddleware(
-  async ({ locals, url, routePattern }, next) => {
-    if (!loggedCollections) {
-      const ids: Record<string, string[]> = {};
-      for (const name of Object.keys(collections)) {
-        ids[name] = [];
-        for (const entry of await getCollection(name)) {
-          ids[name].push(entry.id);
-        }
-      }
+export const onRequest = defineMiddleware(async ({ locals, url }, next) => {
+  locals.lang = process.env.LANG === "fr" ? "fr" : "en";
+  locals.locale = process.env.LOCALE as
+    | `${typeof locals.lang}-${string}`
+    | undefined;
+  locals.buildCommit =
+    // Netlify
+    process.env.COMMIT_REF ||
+    // Cloudflare Pages
+    process.env.CF_PAGES_COMMIT_SHA ||
+    // Fallback
+    "dev";
 
-      console.info("Loaded collections");
-      console.info(JSON.stringify(ids, null, 2));
+  const response = await next();
 
-      const emtns = await getEntry("works", "emoti∗ns");
-      console.info(JSON.stringify({ "emoti∗ns": emtns }, null, 2));
+  if (
+    ["application/json", "text/plain"].includes(
+      response.headers.get("content-type")?.split(";")[0] || "",
+    )
+  ) {
+    return response;
+  }
 
-      loggedCollections = true;
+  const dom = new JSDOM(await response.text(), {
+    url: url.toString(),
+    contentType: response.headers.get("content-type") || "text/html",
+  });
+
+  for (const translatable of dom.window.document.querySelectorAll("[i18n]")) {
+    const key = translatable.textContent?.trim();
+    if (!key) continue;
+
+    const translation =
+      locals.lang === "fr"
+        ? await getEntry("frenchMessages", key)
+        : { data: { msgstr: key } };
+
+    translatable.removeAttribute("i18n");
+    if (translation) {
+      translatable.innerHTML = translation.data.msgstr;
     }
+  }
 
-    locals.lang = process.env.LANG === "fr" ? "fr" : "en";
-    locals.locale = process.env.LOCALE as
-      | `${typeof locals.lang}-${string}`
-      | undefined;
-    locals.buildCommit =
-      // Netlify
-      process.env.COMMIT_REF ||
-      // Cloudflare Pages
-      process.env.CF_PAGES_COMMIT_SHA ||
-      // Fallback
-      "dev";
+  for (const translatable of dom.window.document.querySelectorAll("i18n")) {
+    const key = translatable.innerHTML?.trim();
+    if (!key) continue;
 
-    const response = await next();
+    const translation =
+      locals.lang === "fr" ? await getEntry("frenchMessages", key) : undefined;
 
-    if (
-      ["application/json", "text/plain"].includes(
-        response.headers.get("content-type")?.split(";")[0] || "",
-      )
-    ) {
-      return response;
-    }
+    translatable.outerHTML = translation?.data.msgstr ?? key;
+  }
 
-    const dom = new JSDOM(await response.text(), {
-      url: url.toString(),
-      contentType: response.headers.get("content-type") || "text/html",
-    });
-
-    for (const translatable of dom.window.document.querySelectorAll("[i18n]")) {
-      const key = translatable.textContent?.trim();
-      if (!key) continue;
-
-      const translation =
-        locals.lang === "fr"
-          ? await getEntry("frenchMessages", key)
-          : { data: { msgstr: key } };
-
-      translatable.removeAttribute("i18n");
-      if (translation) {
-        translatable.innerHTML = translation.data.msgstr;
-      }
-    }
-
-    for (const translatable of dom.window.document.querySelectorAll("i18n")) {
-      const key = translatable.innerHTML?.trim();
-      if (!key) continue;
-
-      const translation =
-        locals.lang === "fr"
-          ? await getEntry("frenchMessages", key)
-          : undefined;
-
-      translatable.outerHTML = translation?.data.msgstr ?? key;
-    }
-
-    return new Response(dom.serialize(), {
-      status: 200,
-      headers: response.headers,
-    });
-  },
-);
+  return new Response(dom.serialize(), {
+    status: 200,
+    headers: response.headers,
+  });
+});
